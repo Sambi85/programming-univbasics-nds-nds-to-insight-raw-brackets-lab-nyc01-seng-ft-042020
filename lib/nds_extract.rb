58c1cffb9bdf8145080327fa2d8d8b3a46cf48be
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-26 22:50:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,6 +27,11 @@ dir_name =  "#{directors_database[row_index][:name]}"
 grand_total += directors_database[row_index][:movies][inner_index][:worldwide_gross]
 end
 
+if directors_database[row_index][:name] == directors_database[1][:name] 
+dir_name =  "#{directors_database[row_index][:name]}"
+grand_total += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
   inner_index += 1 
 end
 
