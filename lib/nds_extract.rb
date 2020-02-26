507566c3f19ca3b3ccf5d918323518d993d1b996
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-26 23:05:26 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,88 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+
+grand_total1 = 0
+grand_total2 = 0
+grand_total3 = 0
+grand_total4 = 0
+grand_total5 = 0
+grand_total6 = 0
+grand_total7 = 0
+grand_total8 = 0
+grand_total9 = 0
+row_index = 0
+   
+while row_index < directors_database.length do
+#good, iterates 9 times
+
+inner_index = 0
+inner_len = directors_database[row_index][:movies].length
+#good, iterates 6 times, gives access to :worldwide_gross values
+
+while inner_index < inner_len do
+#good
+
+if directors_database[row_index][:name] == directors_database[0][:name] 
+dir_name1 =  "#{directors_database[row_index][:name]}"
+grand_total1 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[1][:name] 
+dir_name2 =  "#{directors_database[row_index][:name]}"
+grand_total2 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[2][:name] 
+dir_name3 =  "#{directors_database[row_index][:name]}"
+grand_total3 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[3][:name] 
+dir_name4 =  "#{directors_database[row_index][:name]}"
+grand_total4 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[4][:name] 
+dir_name5 =  "#{directors_database[row_index][:name]}"
+grand_total5 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[5][:name] 
+dir_name6 =  "#{directors_database[row_index][:name]}"
+grand_total6 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[6][:name] 
+dir_name7 =  "#{directors_database[row_index][:name]}"
+grand_total7 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[7][:name] 
+dir_name8 =  "#{directors_database[row_index][:name]}"
+grand_total8 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+if directors_database[row_index][:name] == directors_database[8][:name] 
+dir_name9 =  "#{directors_database[row_index][:name]}"
+grand_total9 += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+end
+
+  inner_index += 1 
+end
+
+  row_index += 1 
+end
+result = {
+dir_name1 => grand_total1,
+dir_name2 => grand_total2,
+dir_name3 => grand_total3,
+dir_name4 => grand_total4,
+dir_name5 => grand_total5,
+dir_name6 => grand_total6,
+dir_name7 => grand_total7,
+dir_name8 => grand_total8,
+dir_name9 => grand_total9
+}
+  pp result
 end
