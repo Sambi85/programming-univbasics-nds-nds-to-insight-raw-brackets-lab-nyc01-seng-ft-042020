1f9665c5433a88a3a1b655bbe6de625ec40bda4e
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-26 22:37:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,44 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+
+grand_total = 0
+row_index = 0
+   
+while row_index < directors_database.length do
+#good, iterates 9 times
+
+#column_index = 0
+
+#while column_index < directors_database[row_index].length do
+#good, iterates 2 times
+
+inner_index = 0
+inner_len = directors_database[row_index][:movies].length
+#good, iterates 6 times, gives access to :worldwide_gross values
+
+while inner_index < inner_len do
+#good
+pp grand_total
+
+if directors_database[row_index][:name] == directors_database[0][:name] 
+dir_name =  "#{directors_database[row_index][:name]}"
+grand_total += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+inner_index += 1
+end
+
+  #inner_index += 1 
+end
+
+ # column_index += 1
+#end
+
+  row_index += 1 
+end
+result = {
+dir_name => grand_total
+   
+}
+# pp directors_database
+  pp result
 end
